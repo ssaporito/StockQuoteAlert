767aeef80b35e008a2aaf35ff8837f6d2bea9f55
ssaporito <sandrosaporito@outlook.com>
2023-07-28 18:08:53 -0300

    Configura orquestração dos serviços

--- StockAlertService/Worker.cs
+++ StockAlertService/Worker.cs
@@ -1,4 +1,5 @@
 using Common.Dtos.Stock;
+using System.Security.Cryptography.X509Certificates;
 
 namespace StockAlertService
 {
@@ -15,11 +16,14 @@ namespace StockAlertService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var cmdArgs = Environment.GetCommandLineArgs();
-            var stockName = cmdArgs[1];
-            var sellPrice = decimal.Parse(cmdArgs[2]);
-            var buyPrice = decimal.Parse(cmdArgs[3]);
-            StockMonitorRequest monitorRequest = new(stockName, buyPrice, sellPrice);
+            var exeArgsNullable = GetArgs();
+            if (!exeArgsNullable.HasValue)
+            {                
+                return;
+            }
+
+            var exeArgs = exeArgsNullable.Value;            
+            StockMonitorRequest monitorRequest = new(exeArgs.stockName, exeArgs.buyPrice, exeArgs.sellPrice);
             _stockAlertBroker.DeclareQueues();
             _stockAlertBroker.PublishMonitorRequest(monitorRequest);
 
@@ -29,6 +33,48 @@ namespace StockAlertService
 
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        private (string stockName, decimal sellPrice, decimal buyPrice)? GetArgs()
+        {
+            string stockName;
+            decimal sellPrice, buyPrice;
+
+            string stockArgName = "ARG1";
+            string sellArgName = "ARG2";
+            string buyArgName = "ARG3";
+            string stockArg, sellArg, buyArg;
+
+            var envArgs = Environment.GetEnvironmentVariables();
+            var cmdArgs = Environment.GetCommandLineArgs();
+
+            if (envArgs.Contains(stockArgName) && envArgs.Contains(sellArgName) && envArgs.Contains(buyArgName))
+            {
+                stockArg = (string)envArgs[stockArgName]!;
+                sellArg = (string)envArgs[sellArgName]!;
+                buyArg = (string)envArgs[buyArgName]!;
+            }
+            else if (cmdArgs.Length == 4)
+            {
+                stockArg = cmdArgs[1];
+                sellArg = cmdArgs[2];
+                buyArg = cmdArgs[3];                                
+            } 
+            else
+            {
+                _logger.LogInformation("A aplicação requer os parâmetros stock_name, sell_price e buy_price (e.g. dotnet StockAlertService.dll PETR4 22.67 22.59");
+                return null;
+            }
+
+            stockName = stockArg;
+            bool sellArgOk = decimal.TryParse(sellArg, out sellPrice);
+            bool buyArgOk = decimal.TryParse(buyArg, out buyPrice);
+            if (!sellArgOk || !buyArgOk)
+            {
+                _logger.LogInformation("Os parâmetros de compra e venda devem ser valores decimais (e.g. dotnet StockAlertService.dll PETR4 22.67 22.59");
+            }
+
+            return (stockName, sellPrice, buyPrice);
         }
     }
 }
