121ea6adec61c8b7f3b98e674f016c082a94a422
ssaporito <sandrosaporito@outlook.com>
2023-07-28 16:45:25 -0300

    Explicação para critérios de compra e venda

--- Stock/StockService/StockAlertStrategy.cs
+++ Stock/StockService/StockAlertStrategy.cs
@@ -3,14 +3,16 @@
 namespace StockMonitorService
 {
     public static class StockAlertStrategy
-    {
+    {        
         public static bool ShouldSell(StockMonitorRequest request, decimal? previousPrice, decimal currentPrice)
         {
+            // Garante que a venda só seja recomendada cada vez que o preço transiciona de baixo para cima do limite
             return currentPrice > request.SellPrice && (!previousPrice.HasValue || previousPrice.Value <= request.SellPrice);
         }
 
         public static bool ShouldBuy(StockMonitorRequest request, decimal? previousPrice, decimal currentPrice)
         {
+            // Garante que a compra só seja recomendada cada vez que o preço transiciona de cima para baixo do limite
             return currentPrice < request.BuyPrice && (!previousPrice.HasValue || previousPrice.Value >= request.BuyPrice);
         }
 
